2ddeab389935d9ef99296dc22ff7ef7b2d470216
vaibhavbuccha <jainvaibhav415@gmail.com>
2020-02-08 10:11:21 +0530

    sql

---
+++ database/tbl_celeb.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 08, 2020 at 05:40 AM
+-- Server version: 10.1.34-MariaDB
+-- PHP Version: 7.2.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `movie`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_celeb`
+--
+
+CREATE TABLE `tbl_celeb` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `hitmovies` int(11) NOT NULL,
+  `flopmovies` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_celeb`
+--
+
+INSERT INTO `tbl_celeb` (`id`, `name`, `hitmovies`, `flopmovies`) VALUES
+(1, 'vaibhav', 21, 11);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_celeb`
+--
+ALTER TABLE `tbl_celeb`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_celeb`
+--
+ALTER TABLE `tbl_celeb`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
